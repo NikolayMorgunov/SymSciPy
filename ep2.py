5063b684062c593467472b77902e7abc48b48a09
Darkkvod <da>
2021-11-22 00:31:17 +0300

    Ep2 ready

---
+++ ep2.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import linalg
+import matplotlib.pyplot as plt
+
+filename = input('Введите размер файла small/large: ')
+with open(f'{filename}.txt', 'r') as f:
+    s = list(filter(lambda x: x, f.read().split('\n')))
+
+    N = int(s[0])
+
+    A = np.array([[float(j) for j in i.split()] for i in s[1:N + 1]], dtype=np.float64)
+    b = np.array([[float(i) for i in s[-1].split()]], dtype=np.float64).transpose()
+
+    A_invert = linalg.inv(A)
+    x = (A_invert @ b).transpose()
+    absc = np.arange(1, x[0].size + 1)
+
+    fig, axs = plt.subplots(1, 1)
+    axs.bar(absc, x[0])
+    axs.grid()
+    fig.savefig(f'solved_{filename}.png')
+    plt.show()
